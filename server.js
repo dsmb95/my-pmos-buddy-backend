6a6bfc5429596f6db1e84caa1d46d367d6975da4
dsmb95 <daniellesamanthamananquil@gmail.com>
2026-06-04 14:23:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(
         cookie: {
             maxAge: 1000 * 60 * 60 * 24, 
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: 'none'
         }
     })
